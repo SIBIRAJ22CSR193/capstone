ed9b5cae345aeddd4bbd951dc5255072561b51c2
SIBIRAJ.M <126594998+SIBIRAJ22CSR193@users.noreply.github.com>
2025-03-21 14:44:01 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,8 @@
     echo hi123
     sh 'chmod +x build.sh'
     sh './build.sh'
-    docker login -u ar8888 -p dckr_pat_HU8f1bsQJp3j2DnCttXuuoppo7w
-    docker tag test ar8888/dev
-    docker push ar8888/dev
+    docker login -u sibiraj5181 -p dckr_pat_egJGuz7PjiAU8qJNbxC5yij9T2M
+    docker tag test sibiraj5181/sibi
+    docker push sibiraj5181/sibi
     docker-compose up -d
     
